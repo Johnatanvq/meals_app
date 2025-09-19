fd91e55dbe918f98a8160ce683b6af42b8e60fcf
Johnatan <johnatanv91@gmail.com>
2025-09-19 12:35:23 -0500

    adding meals data on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/pages/categories.dart';
+import 'package:meals_app/pages/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,7 +25,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: CategoriesScreen(),
+      home: MealScreen(
+        title: 'Some category', 
+        meals: dummyMeals,
+      ),
     );
   }
 }
