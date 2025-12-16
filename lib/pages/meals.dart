66fd9f82e78a43d671271b455445e8cf659b764d
johnatan <johnatanv91@gmail.com>
2025-12-16 18:16:40 -0500

    name file change

--- lib/pages/meals.dart
+++ lib/pages/meals.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
-import 'package:meals_app/pages/meal_deatils.dart';
+import 'package:meals_app/pages/meal_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealScreen extends StatelessWidget {
