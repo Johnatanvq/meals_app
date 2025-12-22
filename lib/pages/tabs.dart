dc09ffba0d92fdae2c68e530c84e9c56a7326168
johnatan <johnatanv91@gmail.com>
2025-12-21 19:07:20 -0500

    reading and using returned data

--- lib/pages/tabs.dart
+++ lib/pages/tabs.dart
@@ -46,12 +46,14 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if(identifier == 'filters') {
-      Navigator.of(context).push(MaterialPageRoute(
+      final result = await Navigator.of(context).push<Map <Filter, bool>>(MaterialPageRoute(
         builder: (ctx) => const FiltersScreen())
       );
+
+      print(result);
     }
   }
 
